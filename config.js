e00ebcb0b4c04641ad262adb0e70a8b2e3a8e5a9
Nguyễn Bình Minh <nguyenbinhminh07101997@gmail.com>
2021-06-25 16:28:41 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Người ta thì thanh xuân như một ly trà
+    dăm ba mối tình hết bà thanh xuân,
+    anh đây đậm nhạt cũng được miễn là có em.`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h anh qua đón đi hóng gió nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://m.me/binhminhs10' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
